04ac0b34d4cab46899893de18045a162956069a1
Anastasia <122791968+esvuer@users.noreply.github.com>
2024-02-14 14:26:20 +0000

    fixed aboutcompany, titledtextinput and etc

--- src/Layouts/Page/AboutCompany/Components/General/General.js
+++ src/Layouts/Page/AboutCompany/Components/General/General.js
@@ -1,8 +1,9 @@
 import Profile from "./Components/Profile/Profile";
 import Preferences from "./Components/Preferences";
 function General() {
+    
     return (
-        <div className='d-inline-flex flex-column'>
+        <div className='d-inline-flex col-6 col-sm-3 flex-column'>
             <Profile/> {/* Фото компании, название, тип и адрес */}
             <Preferences/> {/* Инпуты, где можно поменять название, тип и адрес */}
         </div>
